08c04d0335d1eaf0eba6d7b8c7bda985d2cf0ff7
unknown <vaishnaviiipandey2905@gmail.com>
2026-02-04 21:45:32 +0530

    vaishucode

--- firstpython-programme.py
+++ firstpython-programme.py
@@ -27,20 +27,20 @@ print( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # print("======",fact(8))
 
-#take a input from the user and find the square and cube of a number.
+# Take a input from the user and find the square and cube of a number.
 # a = int(input("Enter a number:"))
 # square = a**2
 # cube = a**3
 # print("square is:",{square},'\n' f"cube is:{cube}")
 
-#find the simple interest.
+# Find the simple interest.
 # p = int(input("enter a principal:"))
 # r = int(input("enter interest rate:"))
 # t = int(input("enter a time in years:"))
 # simple_interest = p*r*t/100
 # print("simple_interest:",simple_interest)
 
-#write a program to perform operator using arithmetic operator.
+# Write a program to perform operator using arithmetic operator.
 # a = int(input("enter a first number:"))
 # b = int(input("enter a second number:"))
 
@@ -51,3 +51,31 @@ print( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # print("modulus:",a%b)
 # print("floor divison:",a//b)
 # print("exponential:",a**b)
+
+# Write a program to check wheather a string is a palidrome
+# string = input("enter a string: ")
+# string = string.lower()
+# if string==string[::-1]:
+#     print("this is a palidrome")
+# else:
+#     print("this is not a palidrome")
+
+# Write a program to check wheather a number is even or odd
+# a = int(input("enter a number: "))
+# if a%2==0:
+#     print("This is a even number.")
+# else:
+#     print("This is a odd number.") 
+
+# Write a program to check prime number
+# n = int(input("Enter a number:"))
+
+# if n<=1:
+#     print("Not prime number.")
+# else:
+#     for i in range(2, n):
+#         if n%i==0:
+#             print("Not prime number.")
+#             break
+#     else:
+#         print("Prime number.")            
